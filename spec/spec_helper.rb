a2a9ef60bb2dcc6504a20e3154c92756b7a1b04a
Steven G. Harms <steven@devbootcamp.com>
2015-09-30 18:34:16 -0400

    Change driver to be webkit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
+  Capybara.javascript_driver = :webkit
   config.infer_base_class_for_anonymous_controllers = false
 
   config.order = "random"
