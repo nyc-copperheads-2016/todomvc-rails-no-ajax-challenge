2292106fade9b422f62d0127227d402b89859633
Chris DiMartino <vwchris@gmail.com>
2015-10-01 10:38:50 -0400

    Ran rake rails:update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-ChallengeTodomvcRails::Application.config.session_store :cookie_store, key: '_challenge-todomvc-rails_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# ChallengeTodomvcRails::Application.config.session_store :active_record_store
+Rails.application.config.session_store :cookie_store, key: '_challenge_todomvc_rails_session'
